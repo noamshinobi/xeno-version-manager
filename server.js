f284ecb95759bd9ca7815d37cde6d27507e06a4b
noamshinobi <noam.isenmann@gmail.com>
2026-02-03 19:00:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const ADMIN_PASSWORD = 'mifygzifyzgefizyefgzfe'; // ✅ CHANGEZ CE MOT DE PASSE 
 let versions = [
     { id: '5', number: '1.4', name: 'Temp Ban Fix', description: 'VGC Emulator fixed', enabled: false, downloads: 0 },
     { id: '6', number: '1.5', name: 'Emulator Updt', description: 'VGC Emulator fixed', enabled: false, downloads: 0 },
-    { id: '7', number: '1.6', name: 'BSOD fixes', description: 'blue screen with temp fixed', enabled: true, downloads: 0 },
+    { id: '7', number: '1.6', name: 'BSOD fixes', description: 'blue screen with temp fixed', enabled: false, downloads: 0 },
     { id: '8', number: '1.7', name: 'New Internal Added', description: 'internal dll added', enabled: true, downloads: 0 }
 ];
 
@@ -960,6 +960,7 @@ app.listen(PORT, () => {
 
 // Export pour Vercel
 module.exports = app;
+
 
 
 
