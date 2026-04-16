39a34d33c85ce5156d826b7db4c85c0652115fd0
noamshinobi <noam.isenmann@gmail.com>
2026-04-15 19:03:05 -0700

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,8 @@ let versions = [
     { id: '14', number: '2.0', name: 'newmenu', description: 'emu trop uhq', enabled: false, downloads: 0 },
     { id: '15', number: '2.1', name: 'newmenu', description: 'emu trop uhq', enabled: false, downloads: 0 },
     { id: '16', number: '2.2', name: 'newmenu', description: 'emu trop uhq', enabled: false, downloads: 0 },
-    { id: '17', number: '2.3', name: 'cmd emu et protect', description: 'emu trop uhq', enabled: true, downloads: 0 }
+    { id: '17', number: '2.3', name: 'cmd emu et protect', description: 'emu trop uhq', enabled: false, downloads: 0 },
+    { id: '18', number: '2.4', name: 'cmd emu et protect', description: 'emu trop uhq', enabled: true, downloads: 0 }
 ];
 
 let logs = [];
